e2a328ce1f45581fc8b3d9be007960180a75ff82
sofiaanchondo <anchondosofia78@gmail.com>
2025-11-06 16:30:40 -0600

    Add files via upload

---
+++ EVA3_2_METODOS/src/eva3_2_metodos/EVA3_2_METODOS.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_2_metodos;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sofia
+ */
+public class EVA3_2_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String mensaje;
+        int valor;
+         System.out.println("Captura mensaje: ");
+         mensaje = input.nextLine();
+         System.out.println("Captura repeticiones");
+         valor = input.nextInt();
+         imprimirMensaje(mensaje,valor);
+         
+    }
+            public static void imprimirMensaje(String mensaje, int repeticiones){
+            for(int i = 0; i < repeticiones; i++){
+         System.out.println(mensaje);
+}
+}}
